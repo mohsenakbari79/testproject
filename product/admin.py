33c8515a6d6e869b3c9dd8255c70d28e6dbc665b
mohsen <malevin.git@gmail.com>
2024-08-03 14:29:37 +0330

    set admin panle for django

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from product.models.product import Brand, Phone, Product
+
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("model", "brand")
+    list_filter = ("status", "brand", "contry")
+    search_fields = "model"
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name", "nationality")
+    search_fields = "name"
+
+
+admin.site.register(Brand, BrandAdmin)
+
+
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ("model", "brand")
+    list_filter = ("status", "brand", "contry")
+    search_fields = ("model", "color")
+
+
+admin.site.register(Phone, PhoneAdmin)
